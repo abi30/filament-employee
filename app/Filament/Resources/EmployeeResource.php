1d99dd270a2f275d969f8d083e49d685e67d3d66
abi30 <rakib0751@gmail.com>
2022-08-02 16:10:19 +0200

    done employee again

--- app/Filament/Resources/EmployeeResource.php
+++ app/Filament/Resources/EmployeeResource.php
@@ -3,6 +3,7 @@
 namespace App\Filament\Resources;
 
 use Filament\Forms;
+use App\Models\City;
 use Filament\Tables;
 use App\Models\State;
 use App\Models\Country;
@@ -39,7 +40,8 @@ class EmployeeResource extends Resource
                         Select::make('country_id')
                             ->label('Country')
                             ->options(Country::all()->pluck('name', 'id')->toArray())
-                            ->reactive(),
+                            ->reactive()
+                            ->afterStateUpdated(fn (callable $set) => $set('state_id', null)),
 
                         Select::make('state_id')
                             ->label('State')
@@ -49,8 +51,19 @@ class EmployeeResource extends Resource
                                     return State::all()->pluck('name', 'id');
                                 }
                                 return $country->states->pluck('name', 'id');
+                            })
+                            ->reactive()
+                            ->afterStateUpdated(fn (callable $set) => $set('city_id', null)),
+                        Select::make('city_id')
+                            ->label('City')
+                            ->options(function (callable $get) {
+                                $state = State::find($get('state_id'));
+                                if (!$state) {
+                                    return City::all()->pluck('name', 'id');
+                                }
+                                return $state->cities->pluck('name', 'id');
                             }),
-                        Select::make('city_id')->relationship('city', 'name')->required(),
+
                         Select::make('department_id')->relationship('department', 'name')->required(),
                         TextInput::make('first_name')->required(),
                         TextInput::make('middle_name'),
